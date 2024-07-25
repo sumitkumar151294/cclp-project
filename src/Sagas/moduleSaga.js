84708b4e9e1fea051497afa16c919698786a4a78
Jatin gupta <jatingupta2306.com>
2024-07-25 15:23:02 +0530

    fix

--- src/Sagas/moduleSaga.js
+++ src/Sagas/moduleSaga.js
@@ -1,6 +1,6 @@
 import { call, put, takeLatest } from "redux-saga/effects";
 import { onGetModule, onGetModuleError, onGetModuleSuccess } from "../Store/Slices/moduleSlice";
-import { callModuleApi } from "../Context/ModuleApi";
+import { callModuleApi } from "../Context/moduleApi";
 
 function* Module() {
   try {
