eaf9856c3d116952e521dd06684e3c2fa85d029f
Jatin gupta <jatingupta2306.com>
2024-08-07 17:47:33 +0530

    wEWADSAWEFDSZDCSDZCCDC

--- src/Sagas/index.js
+++ src/Sagas/index.js
@@ -1,42 +1,16 @@
 import { all } from "redux-saga/effects";
-import moduleSaga from "./moduleSaga";
-import loginSaga from "./loginSaga";
-
 import userRoleSaga from "./userRoleSaga";
-import userMasterSaga from "./userMasterSaga";
-
 import translationSaga from "./translationSaga";
-import loginAuthSaga from "./loginAuthSaga";
 import userRoleModulesAccessSaga from "./userRoleModulesAccessSaga";
 import sectionMasterSaga from "./sectionMasterSaga";
 import SectionContentMasterSaga from "./sectionContentMasterSaga";
-import dealCategorySaga from "./dealCategorySaga";
-import dealSaga from "./dealSaga";
-import dealCouponSaga from "./dealCouponSaga";
-import dealCouponCodeSaga from "./dealCouponCodeSaga";
-import uploadSaga from "./uploadSaga";
-import navConfigureSaga from "./navConfigureSaga";
 
 export default function* rootSaga() {
   yield all([
-    moduleSaga(),
-    loginSaga(),
-    loginAuthSaga(),
     translationSaga(),
-
-
-
     userRoleSaga(),
     userRoleModulesAccessSaga(),
-    userMasterSaga(),
-
     sectionMasterSaga(),
     SectionContentMasterSaga(),
-    dealCategorySaga(),
-    dealSaga(),
-    dealCouponSaga(),
-    dealCouponCodeSaga(),
-    uploadSaga(),
-    navConfigureSaga()
   ]);
 }
--- src/Store/StoreReducer.js
+++ src/Store/StoreReducer.js
@@ -1,46 +1,18 @@
 import { combineReducers } from 'redux';
-import  moduleReducer  from './Slices/moduleSlice';
-import loginReducer from './Slices/loginSlice';
-import customerSegmentReducer from './Slices/customerSegementSlice';
-import productContentReducer from './Slices/productContentSlice';
-import productSectionReducer from './Slices/productSectionSlice';
 import userRoleReducer from './Slices/userRoleSlice';
-import userMasterReducer from './Slices/userMasterSlice';
-import clientMasterReducer from './Slices/clientMasterSlice';
-import addCouponReducer from './Slices/addCouponSlice';
 import translationReducer from "./Slices/translationSlice";
-import loginAuthReducer from "./Slices/loginAuthSlice";
 import userRoleModuleAccessReducer from "./Slices/userRoleModuleAccessSlice";
 import  sectionMasterReducer  from './Slices/sectionMasterSlice';
 import sectionContentMasterReducer from './Slices/sectionContentMasterSlice';
-import dealCategoryReducer from './Slices/dealCategorySlice';
-import dealReducer from './Slices/dealSlice';
-import dealCouponReducer from './Slices/dealCouponSlice';
-import dealCouponCodeReducer from './Slices/dealCouponCodeSlice';
-import uploadReducer from './Slices/uploadSlice';
-import navConfigurationReducer from './Slices/NavConfigurationSlice';
+
 
 const reducers = combineReducers({
-    loginReducer:loginReducer,
     translationReducer: translationReducer,
-    loginAuthReducer: loginAuthReducer,
-    moduleReducer: moduleReducer,
-    customerSegmentReducer:customerSegmentReducer,
-    productContentReducer:productContentReducer,
-    productSectionReducer:productSectionReducer,
     userRoleReducer: userRoleReducer,
-    userMasterReducer: userMasterReducer,
-    clientMasterReducer: clientMasterReducer,
-    addCouponReducer:addCouponReducer,
     userRoleModuleAccessReducer: userRoleModuleAccessReducer,
     sectionMasterReducer:sectionMasterReducer,
     sectionContentMasterReducer:sectionContentMasterReducer,
-    dealCategoryReducer:dealCategoryReducer,
-    dealReducer:dealReducer,
-    dealCouponReducer:dealCouponReducer,
-    dealCouponCodeReducer:dealCouponCodeReducer,
-    uploadReducer:uploadReducer,
-    navConfigurationReducer:navConfigurationReducer,
+
 });
 
 export default reducers;
