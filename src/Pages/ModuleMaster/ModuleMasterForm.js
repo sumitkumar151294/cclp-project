ab131a236f08241c90db46eb3c90ecdf72d28c2b
Ankita Singh <ankitafzl27@gmail.com>
2024-08-26 18:36:14 +0530

    module master remaining task almost done

--- src/Pages/ModuleMaster/ModuleMasterForm.js
+++ src/Pages/ModuleMaster/ModuleMasterForm.js
@@ -1,24 +1,28 @@
 import React, { useEffect, useState } from "react";
-import { useFormik } from "formik";
+import { ErrorMessage, Field, Form, Formik } from "formik";
 import * as yup from "yup";
 import Button from "../../Components/Button/Button";
 import { toast, ToastContainer } from "react-toastify";
-import InputField from "../../Components/InputField/InputField";
 import { useDispatch, useSelector } from "react-redux";
 import {
   onGetModule,
   onPostModule,
   onPostModuleReset,
+  onUpdateModuleMaster,
 } from "../../Store/Slices/moduleSlice";
 import Loader from "../../Components/Loader/Loader";
-import ScrollToTop from "../../Components/ScrollToTop/ScrollToTop";
 import { GetTranslationData } from "../../Components/GetTranslationData/GetTranslationData ";
+import Dropdown from "../../Components/Dropdown/Dropdown";
+import { onPostuploadImage, onPostuploadImageReset } from "../../Store/Slices/uploadSlice";
 
-const ModuleMasterForm = () => {
+const ModuleMasterForm = ({ moduleMasterData,setModuleMasterData }) => {
   const [isSubmit, setIsSubmit] = useState(false);
+  const [values, setValues] = useState(null);
   const dispatch = useDispatch();
+
   // to get labels and placeholder from translation
   const submit = GetTranslationData("UIMasterAdmin", "submit");
+  const update = GetTranslationData("UIMasterAdmin", "update");
   const module_name = GetTranslationData("UIMasterAdmin", "module_name");
   const module_master = GetTranslationData("UIMasterAdmin", "module_master");
   const module_name_placeholder = GetTranslationData(
@@ -38,47 +42,157 @@ const ModuleMasterForm = () => {
     "UIMasterAdmin",
     "module_icon_placeholder"
   );
+  const display_order = GetTranslationData("UIMasterAdmin", "display_order");
+  const displayOrderPlaceholder = GetTranslationData(
+    "UIMasterAdmin",
+    "displayOrderPlaceholder"
+  );
+  const display_order_required = GetTranslationData(
+    "UIMasterAdmin",
+    "display_order_required"
+  );
+  const status_label = GetTranslationData("UIMasterAdmin", "status_label");
+  const status_required = GetTranslationData(
+    "UIMasterAdmin",
+    "status_required"
+  );
+  const modul_name_required = GetTranslationData(
+    "UIMasterAdmin",
+    "modul_name_required"
+  );
+  const Module_route_path_required = GetTranslationData(
+    "UIMasterAdmin",
+    "Module_route_path_required"
+  );
+
   // to get module data from the Redux store
   const moduleData = useSelector((state) => state?.moduleReducer);
+  const getwebImage = useSelector(
+    (state) => state.uploadReducer?.postuploadImageData
+  );
+  const uploadImage = useSelector((state) => state.uploadReducer);
   // initial values for the input fields
-  const initialValues = {
+  const [initialValue, setInitialValue] = useState({
     name: "",
     routePath: "",
     icon: "",
+    displayOrder: "",
+    enabled: false, // Default to false (boolean)
+  });
+
+  // reset all fields
+  const reset = {
+    name: "",
+    routePath: "",
+    icon: "",
+    displayOrder: "",
+    enabled: "",
   };
-  // to validate user master form using Yup schema
-  const validateForm = yup.object({
-    name: yup.string().required("Module name is required"),
-    routePath: yup.string().required("Module route path is required"),
+
+  // to validate module master form using Yup schema
+  const validations = yup.object({
+    name: yup.string().required(modul_name_required),
+    routePath: yup.string().required(Module_route_path_required),
+    displayOrder: yup.number().required(display_order_required),
+    enabled: yup.string().required(status_required), // Validate as boolean
   });
-  // to handle form using useFormik hook
-  const { values, errors, touched, handleChange, handleSubmit } = useFormik({
-    initialValues: initialValues,
-    validationSchema: validateForm,
-    onSubmit: (values, action) => {
-      setIsSubmit(true);
-      const updatedValues = {
-        ...values,
-        clientId: 6,
-      };
-      dispatch(onPostModule(updatedValues));
-      action.resetForm();
-    },
-  });
-  //to handle navigation and toast notifications based on module status
+
+  // to handle image changes
+  const handleImageChange = (setFieldValue, event) => {
+    const file = event.currentTarget.files[0];
+    const formData = new FormData();
+    formData.append("file", file);
+    setFieldValue("icon", formData);
+  };
+
+  // to handle form submit
+  const handleSubmit = (values) => {
+    if (values) {
+      if (typeof values.icon === "object") {
+        dispatch(onPostuploadImage(values.icon));
+        setValues(values);
+      } else {
+        const moduleData = {
+          ...values,
+          deleted: false,
+          enabled:
+            typeof values?.enabled === "boolean"
+              ? values.enabled
+              : values?.enabled === "true",
+          clientId: 6,
+          name: values?.name,
+          routePath: values?.routePath,
+          icon: values?.icon,
+          displayOrder: values?.displayOrder,
+          id: values?.id,
+        };
+        dispatch(onUpdateModuleMaster(moduleData));
+        setInitialValue(reset);
+        setModuleMasterData("");
+      }
+    }
+  };
+
+  // options for status
+  const statusOptions = [
+    { value: true, label: "Active" },
+    { value: false, label: "Non Active" },
+  ];
   useEffect(() => {
-    if (isSubmit && moduleData?.status_code === "201") {
-      toast.success(moduleData?.message);
+    if (uploadImage?.post_status_code === "201") {
+      const moduleData = {
+        icon: getwebImage,
+        clientId: 6,
+        deleted: false,
+        name: values?.name,
+        routePath: values?.routePath,
+        displayOrder: values?.displayOrder,
+        ...(moduleMasterData && { id: values.id }),
+      };
+      dispatch(onPostModule(moduleData));
+      setModuleMasterData("");
+    }
+  }, [uploadImage, values]);
+  // useEffect(() => {
+  //   if (moduleMasterData) {
+  //     setInitialValue({
+  //       name: moduleMasterData?.name || "",
+  //       routePath: moduleMasterData?.routePath || "",
+  //       icon: moduleMasterData?.icon || "",
+  //       displayOrder: moduleMasterData?.displayOrder || "",
+  //       enabled: moduleMasterData?.enabled ?? false, // Ensure boolean
+  //     });
+  //     window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
+  //   }
+  // }, [moduleMasterData]);
+
+  // to handle navigation and toast notifications based on module master status
+  useEffect(() => {
+    if (moduleData?.status_code === "201") {
+      toast.success(moduleData.message);
+      dispatch(onPostuploadImageReset());
+      dispatch(onPostModuleReset());
+      dispatch(onGetModule());
+    } else if (moduleData?.update_status_code === "205") {
+      toast.success(moduleData?.updateMessage);
       dispatch(onGetModule());
       dispatch(onPostModuleReset());
-    } else if (isSubmit && moduleData?.status_code) {
-      toast.error(moduleData?.message);
+      setInitialValue(reset);
+    }else if (moduleData?.status_code) {
+      toast.error(moduleData.message);
       dispatch(onPostModuleReset());
     }
-  }, [moduleData]);
+    setIsSubmit(false); // reset the submit state after processing
+  }, [moduleData, isSubmit]);
+  useEffect(() => {
+    if (moduleMasterData) {
+      window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
+      setInitialValue(moduleMasterData);
+    }
+  }, [moduleMasterData]);
   return (
     <>
-      <ScrollToTop />
+      <ToastContainer />
       <div className="containers-fluid">
         <div className="row">
           <div className="col-xl-12 col-xxl-12">
@@ -92,82 +206,134 @@ const ModuleMasterForm = () => {
                     <Loader classType={"absoluteLoader"} />
                   </div>
                 ) : (
-                  <div className="containers-fluid">
-                    <form onSubmit={handleSubmit}>
-                      <div className="row">
-                        <div className="col-sm-4 form-group mb-2">
-                          <label htmlFor="name-f">
-                            {module_name}
-                            <span className="text-danger">*</span>
-                          </label>
-                          <InputField
-                            type="text"
-                            className={`form-control ${
-                              errors.name && touched.name ? "is-invalid" : ""
-                            }`}
-                            name="name"
-                            id="name-f"
-                            placeholder={module_name_placeholder}
-                            value={values.name}
-                            onChange={handleChange}
-                          />
-                          {errors.name && touched.name && (
-                            <p className="error-message">{errors.name}</p>
-                          )}
+                  <Formik
+                    initialValues={initialValue}
+                    validationSchema={validations}
+                    onSubmit={handleSubmit}
+                    enableReinitialize={true}
+                  >
+                    {({ errors, touched, setFieldValue }) => (
+                      <Form>
+                        <div className="row">
+                          <div className="col-sm-4 form-group mb-2">
+                            <label htmlFor="name-f">
+                              {module_name}
+                              <span className="text-danger">*</span>
+                            </label>
+                            <Field
+                              type="text"
+                              className={`form-control ${
+                                errors.name && touched.name ? "is-invalid" : ""
+                              }`}
+                              name="name"
+                              id="name-f"
+                              placeholder={module_name_placeholder}
+                            />
+                            <ErrorMessage
+                              name="name"
+                              component="div"
+                              className="error-message"
+                            />
+                          </div>
+                          <div className="col-sm-4 form-group mb-2">
+                            <label htmlFor="routePath">
+                              {module_route_path}
+                              <span className="text-danger">*</span>
+                            </label>
+                            <Field
+                              type="text"
+                              className={`form-control ${
+                                errors.routePath && touched.routePath
+                                  ? "is-invalid"
+                                  : ""
+                              }`}
+                              name="routePath"
+                              id="routePath"
+                              placeholder={route_path_placeholder}
+                            />
+                            <ErrorMessage
+                              name="routePath"
+                              component="div"
+                              className="error-message"
+                            />
+                          </div>
+                          <div className="col-sm-4 form-group mb-2">
+                            <label htmlFor="icon">
+                              {module_icon}
+                              <span className="text-danger">*</span>
+                            </label>
+                            <input
+                              type="file"
+                              name="icon"
+                              className={`form-control ${
+                                errors.icon && touched.icon
+                                  ? "is-invalid"
+                                  : ""
+                              }`}
+                              onChange={(event) =>
+                                handleImageChange(setFieldValue, event)
+                              }
+                            />
+                            <ErrorMessage
+                              name="icon"
+                              component="div"
+                              className="error-message"
+                            />
+                          </div>
+                          <div className="col-sm-4 form-group mb-2">
+                            <label htmlFor="displayOrder">
+                              {display_order}
+                              <span className="text-danger">*</span>
+                            </label>
+                            <Field
+                              type="number"
+                              name="displayOrder"
+                              className={`form-control ${
+                                errors.displayOrder && touched.displayOrder
+                                  ? "is-invalid"
+                                  : ""
+                              }`}
+                              id="displayOrder"
+                              placeholder={displayOrderPlaceholder}
+                            />
+                            <ErrorMessage
+                              name="displayOrder"
+                              component="div"
+                              className="error-message"
+                            />
+                          </div>
+                          <div className="col-sm-4 form-group mb-2">
+                            <label>
+                              {status_label}
+                              <span className="text-danger">*</span>
+                            </label>
+                            <Field
+                              name="enabled"
+                              component={Dropdown}
+                              options={statusOptions}
+                              className={`form-select ${
+                                errors.enabled && touched.enabled
+                                  ? "is-invalid"
+                                  : ""
+                              }`}
+                            />
+                            <ErrorMessage
+                              name="enabled"
+                              component="div"
+                              className="error-message"
+                            />
+                          </div>
                         </div>
-                        <div className="col-sm-4 form-group mb-2">
-                          <label htmlFor="description">
-                            {module_route_path}
-                            <span className="text-danger">*</span>
-                          </label>
-                          <InputField
-                            type="text"
-                            className={`form-control ${
-                              errors.routePath && touched.routePath
-                                ? "is-invalid"
-                                : ""
-                            }`}
-                            name="routePath"
-                            id="description"
-                            placeholder={route_path_placeholder}
-                            value={values.routePath}
-                            onChange={handleChange}
+                        <div className="col-sm-4 mb-4">
+                          <Button
+                            text={moduleMasterData ? update : submit}
+                            end_icon="fa fa-arrow-right"
+                            className="btn btn-primary pad-aa mt-2"
                           />
-                          {errors.routePath && touched.routePath && (
-                            <p className="error-message">{errors.routePath}</p>
-                          )}
                         </div>
-                        <div className="col-sm-4 form-group mb-2">
-                          <label htmlFor="description">
-                            {module_icon}
-                            <span className="text-danger">*</span>
-                          </label>
-                          <InputField
-                            className={`form-control ${
-                              errors.icon && touched.icon ? "is-invalid" : ""
-                            }`}
-                            type="text"
-                            name="icon"
-                            id="flexCheckDefault2"
-                            placeholder={module_icon_placeholder}
-                            value={values.icon}
-                            onChange={handleChange}
-                          />
-                          {errors.icon && touched.icon && (
-                            <p className="error-message">{errors.icon}</p>
-                          )}
-                        </div>
-                      </div>
-                      <div className="col-sm-4 mb-4">
-                        <Button
-                          text={submit}
-                          end_icon="fa fa-arrow-right"
-                          className="btn btn-primary  pad-aa mt-2"
-                        />
-                        <ToastContainer />
-                      </div>
-                    </form>
-                  </div>
+                      </Form>
+                    )}
+                  </Formik>
                 )}
               </div>
             </div>
